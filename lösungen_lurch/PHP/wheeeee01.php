d6afe7daa96bb28aec2052d3a535d65643b72d9e
raketenlurch <raketenlurch@riseup.net>
2021-05-28 04:29:09 +0200

    Add wheeeee01.php

---
+++ lösungen_lurch/PHP/wheeeee01.php
@@ -0,0 +1,21 @@
+<?php
+
+function duplicateHashtag($amount) {
+	$hashtags = '';
+	for ($i = 0; $i < $amount; $i++) {
+		$hashtags .= "#";
+	}
+
+	return $hashtags;
+}
+
+// user input
+$number = (int)readline("Enter MAX:");
+
+$result = '';
+
+for ($i = 1; $i <= $number; $i++) {
+	$result = duplicateHashtag($i);
+	print $result;
+	print "\n";
+}
