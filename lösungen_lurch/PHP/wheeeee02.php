b35458f18076afd2b312c34ce63e13f0278525e2
raketenlurch <raketenlurch@riseup.net>
2021-05-28 17:21:40 +0200

    Add first version of wheeeee02.php

---
+++ lösungen_lurch/PHP/wheeeee02.php
@@ -0,0 +1,34 @@
+<?php
+
+function duplicateHashtag($amount) {
+	$hashtags = '';
+	for ($i = 0; $i < $amount; $i++) {
+		$hashtags .= "#";
+	}
+
+	return $hashtags;
+}
+
+function decreaseHashtags($amount) {
+	$hashtags = $amount;
+	for ($i = ) {
+	
+	}
+
+	return $hashtags;	
+}
+
+// user input
+$number = (int)readline("Enter MAX:");
+
+$result = '';
+
+for ($i = 1; $i <= $number; $i++) {
+	$result = duplicateHashtag($i);
+	print $result;
+	print "\n";
+}
+
+for () {
+
+}
