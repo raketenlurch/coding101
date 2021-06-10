48da236c4774b5f0e3a6e97e237cc963d45eca73
raketenlurch <raketenlurch@riseup.net>
2021-06-10 03:29:36 +0200

    Update matrix03.py

--- lösungen_lurch/Python/matrix03.py
+++ lösungen_lurch/Python/matrix03.py
@@ -39,25 +39,27 @@ print_matrix_random_characters(width, height, randoms)
 """
 # Lösung ohne Kommentare
 
+
 def print_matrix_random_characters(width, height, random_character):
     digits_of_rectangle = width * height
     random_digits = []
 
-    while len(random_digits) <= random_character-1:
-        random_position = random.randint(0, digits_of_rectangle)
+    while len(random_digits) < random_character:
+        random_position = random.randrange(0, digits_of_rectangle)
 
         if random_position in random_digits:
             random_digits.remove(random_position)
 
         random_digits.append(random_position)
 
-    for i in range(1, digits_of_rectangle+1):
+    for i in range(0, digits_of_rectangle):
         if i in random_digits:
             print("*", end="")
         else:
             print("#", end="")
 
-        if i % width == 0:
+        # Lässt sich mathematisch bestimmt noch schöner lösen
+        if (i+1) % width == 0:
             print()
 
 
